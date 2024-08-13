bbeeebf56f7e4c0aa131633fd3c05dacc1aa82a7
Auto Logon <autologon@CSED.LAB>
2024-08-12 21:16:42 -0300

    vai corinthians

--- Projeto 1/src/Main.kt
+++ Projeto 1/src/Main.kt
@@ -1,5 +1,26 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-   println("Olá Mundo")
+   //criando variaveis
+//
+//   var name : String = "pedro"
+//   var nameSemTipo = "kauan";
+//
+//// val = constante, dica: colocar em maiuscula
+//  val NOME = "corinthians";
+//
+//   println(nameSemTipo);
+//   println(NOME);
+//   println("É o time do povo: ${NOME}")
+
+
+   val valor1: Int = 100;
+   val valor2: Int = 50;
+
+   //PODE SER NULO OU NÃO
+
+   var pratos: Int;
+
+   pratos = readln()!!.toInt();
+
 }
